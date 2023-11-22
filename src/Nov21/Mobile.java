08d67c4bc46e487964c80d98aa1bfc157e44a3b1
pankaj501 <pankajbandhan39@gmail.com>
2023-11-21 21:29:21 -0500

    test

---
+++ src/Nov21/Mobile.java
@@ -0,0 +1,16 @@
+package Nov21;
+
+public class Mobile {
+
+    String deviceType=null;
+    int memory=0;
+    public void call(){
+        System.out.println("calling");
+    }
+    public void text(){
+        System.out.println("Texting");
+    }
+    public void alarm(){
+        System.out.println("Snoozing");
+    }
+}
---
+++ src/Nov21/Objects.java
@@ -0,0 +1,10 @@
+package Nov21;
+
+public class Objects {
+    public static void main(String[] args) {
+        Mobile mobile=new Mobile();
+        System.out.println(mobile);
+    //mobile.call();
+    //mobile.deviceType="Apple";
+    }
+}
